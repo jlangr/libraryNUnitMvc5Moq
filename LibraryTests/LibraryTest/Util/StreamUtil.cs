0f3f6265c1a872781e338fdd824e3860bc6a5331
Jeff Langr <jeff@langrsoft.com>
2017-06-05 15:01:53 -0600

    shell test for FileReport example

---
+++ LibraryTests/LibraryTest/Util/StreamUtil.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+
+namespace LibraryTests.LibraryTest.Util
+{
+    public class StreamUtil
+    {
+        public static Stream StreamFromString(string s)
+        {
+            var stream = new MemoryStream();
+            var writer = new StreamWriter(stream);
+            writer.Write(s);
+            writer.Flush();
+            stream.Position = 0;
+            return stream;
+        }
+    }
+}
---
+++ LibraryTests/Reporting/FileReportTest.cs
@@ -0,0 +1,13 @@
+﻿using Library.Reporting;
+using LibraryTests.LibraryTest.Util;
+using NUnit.Framework;
+using System;
+using System.IO;
+
+namespace LibraryTests.Library.Reporting
+{
+    public class FileReportTest
+    {
+        readonly string Eol = Environment.NewLine;
+    }
+}
