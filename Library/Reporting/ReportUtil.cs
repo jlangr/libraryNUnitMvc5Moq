f15f1278792361928c012bda170a4b5a23475d8c
Jeff Langr <jeff@langrsoft.com>
2017-06-05 21:29:36 -0600

    move reportutil and save project

--- Library/Reporting/ReportUtil.cs
+++ Library/Reporting/ReportUtil.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Web;
+﻿using System.Text;
 
-namespace Library.Util
+namespace Library.Reporting
 {
     public class ReportUtil
     {
