79fa773a976742719d593fae2d30cc69fb125da1
Jeff Langr <jeff@langrsoft.com>
2017-07-10 16:27:18 -0600

    3: use getter on date of last transaction

--- Library/Util/Portfolio.cs
+++ Library/Util/Portfolio.cs
@@ -15,9 +15,18 @@ namespace Library.Util
         public bool IsEmpty { get { return UniqueSymbolCount == 0;  } }
         public int UniqueSymbolCount { get { return Holdings.Count; } }
 
-        public DateTime DateOfLastTransaction { get; private set; }
         private IList<Transaction> Transactions { get; set; }
 
+        public DateTime DateOfLastTransaction
+        {
+            get
+            {
+                return Transactions
+                    .Reverse()
+                    .First()
+                    .Date;
+            }
+        }
         public DateTime LastTransactionDate(string symbol)
         {
             return Transactions
@@ -28,9 +37,8 @@ namespace Library.Util
 
         private void Transact(string symbol, int shares)
         {
-            DateOfLastTransaction = TimeService.Now;
             Holdings[symbol] = Shares(symbol) + shares;
-            Transactions.Add(new Transaction(symbol, shares, DateOfLastTransaction));
+            Transactions.Add(new Transaction(symbol, shares, TimeService.Now));
         }
 
         public void Purchase(string symbol, int shares)
