28de410ff7459b92bdab09d797e7f08f96ed471f
Jeff Langr <jeff@langrsoft.com>
2017-07-11 13:12:55 -0600

    stock service

---
+++ Library/Util/StockService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Library.Util
+{
+    public interface IStockService
+    {
+        decimal Price(string symbol);
+    }
+}
